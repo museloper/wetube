136c8e7fb6aae4a3b0bd4c5e2802df372aebcc80
museloper <nosf6842@naver.com>
2022-07-12 10:24:44 +0900

    GET Request

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const PORT = 4000;
 // 서버 생성
 const app = express();
 
+// GET Request
+app.get("/", () => console.log("Somebody is trying to go home 🙊"));
+
 app.listen(PORT, () =>
   console.log(`Server listening on http://localhost:${PORT} 🚀`)
 );
